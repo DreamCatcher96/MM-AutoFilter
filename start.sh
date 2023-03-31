be1f2c25ceeb0302aa458d3ed8bf856cdf379c65
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2023-03-31 22:53:27 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/jikkubot32/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/jikkubot32/DQ-Join.git /DQ-Join
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /DQ-Join
 fi
-cd /DQ-The-File-Donor
+cd /DQ-Join
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting DQ-Join...."
 python3 bot.py
