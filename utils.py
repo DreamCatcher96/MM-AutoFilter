e5e046bc54697497d430c6f61c58d6e55bb0637f
jikkubot32 <93936137+jikkubot32@users.noreply.github.com>
2023-04-01 11:16:21 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ import os
 from datetime import datetime, date
 import string
 from typing import List
+from database.fsub_db import Fsub_DB
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
@@ -47,6 +48,13 @@ class temp(object):
     SETTINGS = {}
 
 async def is_subscribed(bot, query):
+    if not AUTH_CHANNEL:
+        return True
+    user = await Fsub_DB().get_user(query.from_user.id)
+    if user:
+        return True
+    else:
+        pass
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
@@ -54,7 +62,7 @@ async def is_subscribed(bot, query):
     except Exception as e:
         logger.exception(e)
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if user.status != 'kicked':
             return True
 
     return False
